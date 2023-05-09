9e1280ea014ce361c61700ca4931860b6de15263
franck <carnet.franck.paul@gmail.com>
2023-05-09 15:36:19 +0200

    Update author, render to Caesar what belongs to Caesar

--- src/My.php
+++ src/My.php
@@ -5,9 +5,9 @@
  * @package Dotclear
  * @subpackage Plugins
  *
- * @author Franck Paul and contributors
+ * @author Jean-Christian Denis, Franck Paul and contributors
  *
- * @copyright Franck Paul carnet.franck.paul@gmail.com
+ * @copyright Jean-Christian Denis, Franck Paul
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
 declare(strict_types=1);
