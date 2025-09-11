85b2d1751703d99251b4d31400d135cd86f9bbda
franck <carnet.franck.paul@gmail.com>
2025-09-11 09:50:58 +0200

    Code review

--- src/My.php
+++ src/My.php
@@ -15,7 +15,6 @@ declare(strict_types=1);
 
 namespace Dotclear\Plugin\_template_;
 
-use Dotclear\App;
 use Dotclear\Module\MyPlugin;
 
 /**
@@ -23,37 +22,4 @@ use Dotclear\Module\MyPlugin;
  */
 class My extends MyPlugin
 {
-    /**
-     * Check permission depending on given context
-     *
-     * @param      int   $context  The context
-     *
-     * @return     null|bool  null if not relevant, true if allowed, else false
-     */
-    protected static function checkCustomContext(int $context): ?bool
-    {
-        return match ($context) {
-            // Limit backend to content admin and pages user
-            self::BACKEND, self::MANAGE, self::MENU, self::WIDGETS => App::task()->checkContext('BACKEND')
-                && App::blog()->isDefined()
-                && App::auth()->check(App::auth()->makePermissions([
-                    App::auth()::PERMISSION_CONTENT_ADMIN,
-                    App::auth()::PERMISSION_ADMIN,
-                ]), App::blog()->id()),
-
-            default => null,
-        };
-    }
-
-    /**
-     * Makes an url including optionnal parameters.
-     *
-     * @param      array   $params  The parameters
-     *
-     * @deprecated since 2.27, use My::manageUrl($param)
-     */
-    public static function makeUrl(array $params = []): string
-    {
-        return static::manageUrl($params);
-    }
 }
